f2a7b875dc4c043cdca15572817f615352f243eb
Neander de Souza <neander.faria@hotmail.com>
2023-07-18 14:26:03 -0300

    Testes automatizados - Configurando banco de testes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,6 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 
 import { app } from '../src/app'
 
@@ -10,6 +11,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npx knex migrate:rollback --all')
+    execSync('npx knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
