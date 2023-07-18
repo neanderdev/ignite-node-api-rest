5c79ed8b29d77a011e6439dafe55ccfb9dda67a4
Neander de Souza <neander.faria@hotmail.com>
2023-07-18 10:52:26 -0300

    Implementando rotas - Tipagem no Knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+import 'knex'
+// ou faça apenas:
+// import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
+    }
+  }
+}
